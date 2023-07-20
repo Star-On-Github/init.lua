3e52217deb76d75c4bdbac261bad0d6712dd1626
Omar Mohamed <omarcoptan9@gmail.com>
2023-07-20 22:36:50 +0300

    Initial nvim config
    * Neovim is fun

---
+++ README.md
@@ -0,0 +1,28 @@
+# Nvim dotfiles
+
+My dotfiles for neovim
+
+## Table of contents
+
+* [Requirments](#requirments)
+* [How to Install](#how-to-install)
+* [Features](#features)
+
+## Requirments
+
+1. Install `ripgrep`
+
+## How to Install
+
+1. Clone this repo(```git clone https://github.com/Coptan99/init.lua .config/nvim```) (```rm -rf ~/.config/nvim``` **if you had it before**)
+2. Run nvim
+3. Run `:PackerSync` (This will install all required plugins)
+
+## Features
+
+1. Plugin manager [Packer](https://github.com/wbthomason/packer.nvim)
+2. Good theme [rose-pine](https://github.com/rose-pine/neovim)
+3. Diagnostics [Trouble](https://github.com/folke/trouble.nvim)
+4. Fuzzy finder [Telescope](https://github.com/nvim-telescope/telescope.nvim)
+6. Git integration [Fugitive](https://github.com/tpope/vim-fugitive)
+7. Statusline [Lualine](https://github.com/nvim-lualine/lualine.nvim) (**disabled by default**)
---
+++ lua/coptan/packer.lua
@@ -0,0 +1,82 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd.packadd 'packer.nvim'
+
+return require 'packer'.startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    -- Fuzzy finder
+    use({
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.0",
+        requires = { { "nvim-lua/plenary.nvim" } },
+    })
+
+    use { "catppuccin/nvim", as = "catppuccin" }
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
+    use 'lukas-reineke/indent-blankline.nvim'
+    use({
+        "folke/trouble.nvim",
+        config = function()
+            require 'trouble'.setup {
+                icons = false,
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    })
+
+    use({
+        "nvim-treesitter/nvim-treesitter",
+        run = function()
+            local ts_update = require 'nvim-treesitter.install'.update({ with_sync = true })
+            ts_update()
+        end,
+    })
+    use 'nvim-treesitter/playground'
+    use 'theprimeagen/harpoon'
+    use 'mbbill/undotree'
+    use 'tpope/vim-fugitive'
+    use 'nvim-treesitter/nvim-treesitter-context'
+
+    use({
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v1.x",
+        requires = {
+            -- LSP Support
+            { "neovim/nvim-lspconfig" },
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
+
+            -- Autocompletion
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "saadparwaiz1/cmp_luasnip" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-nvim-lua" },
+
+            -- Snippets
+            { "L3MON4D3/LuaSnip" },
+            { "rafamadriz/friendly-snippets" },
+        },
+    })
+
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+    })
+    use 'mzlogin/vim-markdown-toc'
+
+    use 'brenoprata10/nvim-highlight-colors'
+    use 'ziontee113/color-picker.nvim'
+
+    use 'numToStr/Comment.nvim'
+    -- use 'kyazdani42/nvim-web-devicons'
+    use 'folke/todo-comments.nvim'
+end)
