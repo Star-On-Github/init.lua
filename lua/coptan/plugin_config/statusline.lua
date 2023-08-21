f84c9976f4e8609b449340b542dd76e21bebb798
Omar Mohamed <omarcoptan9@gmail.com>
2023-08-21 14:58:07 +0300

    Customize lualine

--- lua/coptan/plugin_config/statusline.lua
+++ lua/coptan/plugin_config/statusline.lua
@@ -1,7 +1,12 @@
 require('lualine').setup {
     options = {
         icons_enabled = true,
+        component_separators = '|',
+        section_separators = '',
     },
+    sections = {
+        lualine_x = {}
+    }
 }
 require("fidget").setup {
     text = {
